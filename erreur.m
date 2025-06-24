336b6ee2b6c6696294ba1ab9ff2b17f5b680803f
marionlhrmt <159469455+marionlhrmt@users.noreply.github.com>
2025-06-24 15:20:43 +0200

    Update erreur.m

--- erreur.m
+++ erreur.m
@@ -1,29 +1,11 @@
 function erreur = calculerMatriceErreur(imageReelle, imagePredite)
-% CALCULERMATRICEERREUR Calcule la matrice d'erreur entre deux images RGB 
-% représentées sous forme de cellules (chaque cellule contient [R G B]) /  Calculates the error matrix between two RGB images 
-% represented as cells (each cell contains [R G B])
-%
-%   erreur = calculerMatriceErreur(imageReelle, imagePredite)
-%
-%   Output :
-%       erreur : matrice HxW de cellules contenant [dR dG dB] / error: HxW matrix of cells containing [dR dG dB].
 
-    [H, W] = size(imageReelle);
 
-    % Vérification des dimensions
     if ~isequal(size(imageReelle), size(imagePredite))
-        error('Les dimensions des deux images doivent être identiques.');
+        error('Les images doivent avoir la même taille.');
     end
 
-    erreur = cell(H, W);
-
-    for i = 1:H
-        for j = 1:W
-            pixelReel = double(imageReelle{i, j});
-            pixelPrevu = double(imagePredite{i, j});
-            erreur{i, j} = pixelReel - pixelPrevu; % Donne [dR dG dB]
-        end
-    end
+    erreur = double(imageReelle) - double(imagePredite);
 end
 
 function H = calc_entropie(image)
